59fed4a2ae796eb6fd6bb25e9f13ea6586eac59f
ozan-k <ozan.kah@gmail.com>
2023-03-31 00:10:44 +0200

    exercise2

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class Main {
@@ -10,48 +11,33 @@ public class Main {
     Dog d4 = new Dog("Duke", 11);
 
     dogs = new ArrayList<>();
+
+    dogs.add(d1);
     dogs.add(d1);
     dogs.add(d2);
+    dogs.add(d2);
     dogs.add(d3);
+    dogs.add(d3);
+    dogs.add(d4);
     dogs.add(d4);
 
     System.out.println(dogs);
+    // => [(King, 5), (King, 5), (Rex, 7), (Rex, 7), (Boss, 2), (Boss, 2), (Duke, 11), (Duke, 11)]
+
+    removeDuplicates(dogs);
+    System.out.println(dogs);
     // => [(King, 5), (Rex, 7), (Boss, 2), (Duke, 11)]
-    printIfOlderThanTen(dogs);
-    // => Dogs older than 10:
-    // => (Duke, 11)
-    printIfOlderThanAverage(dogs);
-    // => Dogs older than 6.25 (average):
-    // => (Rex, 7)
-    // => (Duke, 11)
   }
 
-  private static void printIfOlderThanTen(ArrayList<Dog> dogs) {
-       System.out.println("Dogs older than " + 10 + ":");
-       for (Dog dog : dogs) {
-          if (dog.getAge() > 10) {
-             System.out.println(dog);
-             }
-       }
-    }
-
-
-  private static void printIfOlderThanAverage(ArrayList<Dog> dogs) {
-      double average = getAverage(dogs);
-      System.out.println("Dogs older than " + average + "(average):");
-      for (Dog dog : dogs){
-          if (dog.getAge() > average){
-              System.out.println(dog);
-          }
-      }
-
+  static void removeDuplicates(ArrayList<Dog> dogs) {
+     ArrayList<Dog> result = new ArrayList<>();
+     for (Dog dog : dogs){
+         if (!(result.contains(dog))){
+            result.add(dog);
+         }
+     }
+     dogs.clear();
+     dogs.addAll(result);
   }
 
-  private static double getAverage(ArrayList<Dog> dogs) {
-    double sum = 0;
-    for (Dog dog : dogs){
-        sum += dog.getAge();
-    }
-    return sum / dogs.size();
-  }
 }
